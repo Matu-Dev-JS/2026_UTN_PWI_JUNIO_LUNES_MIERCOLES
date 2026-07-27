8e341c677b5b22652b24dc5614800fc9e3e80eaf
matu.dev <mati.a.gimenez2002@gmail.com>
2026-07-27 11:05:09 -0300

    Clase 12 JS

---
+++ Clase 12 JS/script.js
@@ -0,0 +1,153 @@
+console.log('hola \'mundo')
+
+//TIPOS DE DATOS Primitivos
+//Numericos
+16
+7
+-40
+1.2
+-1.2
+
+//String
+'hola'
+'mati'
+'1'
+"que tal?"
+"Hola\""
+
+//Booleanos
+true
+false
+
+//null
+null
+
+//undefined
+undefined
+
+
+
+//Constructores/Transformadores
+
+Number('1') //1
+Number('hola') //NaN
+
+String(50) //'50'
+
+
+//Si boolean devuelve verdadero decimos que un dato es Truthy 
+Boolean(50) //true
+
+//Sino es falsy
+//Falsy son todos los datos que al pasarlos a boolean dan false
+Boolean(0) //false
+Boolean(null) 
+Boolean(undefined)
+Boolean('')
+Boolean(NaN)
+
+
+
+//Comparadores
+//Nos permiten comparar datos
+//Siempre devuelven booleanos
+
+//Es igual a: ==
+'mati' == 'mati'
+1 == '1'
+
+//Es estrictamente igual a: === (RECOMENDABLE)
+1 === '1' //False 
+
+//Es distinto de: !=
+1 != 2
+1 != '1'
+
+//Es estrictamente distinto de: !==
+1 !== '1'
+
+//Es mayor a: >
+//Es menor a: <
+//Es mayor igual: >=
+//Es menor igual: <=
+
+
+//Excepcion de NaN
+//NaN Es un dato UNICO, es el unico dato que es incomparable, nisiquiera consigo mismo
+NaN == NaN // false
+
+//La forma correcta de saber si un valor es NaN es mediante la funcion nativa isNaN()
+isNaN('hola') //true
+isNaN('1') //False
+
+
+//Operadores logicos
+
+//NOT: !
+//Podemos hacer negaciones
+//Devolvera el valor booleano opuesto al valor negado
+/* !(true) //false
+!('hola') //false
+!(Number('hola')) //true
+
+esDeDia = true
+esDeNoche = !esDeDia */
+
+//OR
+//Para hacer condiciones compuestas
+//Va a seleccionar un valor entre 2 valores
+//Si el primer valor es Truthy seleccionara el primero
+//Si el primer valor es Falsy seleccionara el segundo
+
+70 || 90 //70
+Number('8') || null //8
+null || undefined //undefined
+
+//Contesto la pregunta si estoyLibre o esHorarioLaboral
+//contestoPregunta = estoyLibre || esHorarioLaboral
+
+//AND
+//Para hacer condiciones compuestas
+//Va a seleccionar un valor entre 2 valores
+//Si el primer valor es Truthy seleccionara el segundo
+//Si el primer valor es Falsy seleccionara el primero
+
+0 && true //0
+false && null //false
+50 && undefined //undefined
+
+
+//Operadores aritmeticos
+
+//Concatenacion: +
+//Ocurre cuando hay un string en la operacion
+//Transforma cualquier valor no string a string y concatena (unifica las cadenas)
+//SIEMPRE DEVUELVE UN STRING
+50 + 'hola'
+//String(50) + 'hola
+//'50' + 'hola' = '50hola'
+
+'1' + 1 //'11'
+
+null + '1' //'null1'
+
+
+//Suma: +
+//Ocurre cuando NO hay un string en la operacion
+//Transforma cualquier valor no numerico en numero y suma
+//siempre devuelve numero
+
+
+40 + 60 //100
+true + 1 //2
+50 + Number('hola') //NaN
+
+//Resta: -
+//Transforma cualquier valor no numerico en numero y resta
+//siempre devuelve numero
+50 - '1' //49
+
+//Division: /
+//Multiplicacion: *
+
+//Cualquier operacion (excepto la concatenacion con NaN da NaN)
