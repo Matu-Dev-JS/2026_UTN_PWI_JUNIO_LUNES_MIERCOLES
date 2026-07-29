cf1e2515e60c80020bb421e37b6b3c7459c84015
matu.dev <mati.a.gimenez2002@gmail.com>
2026-07-29 10:44:49 -0300

    Clase 13

--- Clase 13 JS/script.js
+++ Clase 13 JS/script.js
@@ -6,7 +6,7 @@ IF: el if permite controlar si un bloque de codigo se ejecuta o no bajo una cier
 ELSE: Va siempre seguido de un IF o ELSE IF y se ejecuta cuando a la condicion de este NO sea verdadera
 ELSE IF: Va siempre seguido de un IF o ELSE IF y te permite agregar una condicion cuando la condicion de este falle
 */
-
+/* 
 let password = prompt('ingrese su contraseña')
 
 let contraseniaCorrecta = password === 'pepe_123'
@@ -16,9 +16,9 @@ if(contraseniaCorrecta){
 }
 else{
     console.log("Credenciales invalidas")
-}
+} */
 
-
+/* 
 let numero_dia = 2
 
 if(numero_dia === 0){
@@ -43,7 +43,7 @@ else{
     console.log("Hoy es domingo")
 }
 
-
+ */
 /* 
 Solicitar al usuario su edad (Imaginemos para practicos que el dato siempre es valido (un numero positivo))
 Si la edad del usuario es menor a 16 entonces indicar por consola "Es menor de edad"
@@ -53,3 +53,40 @@ Si la edad del usuario es mayor a 18 entonces indicar por consola "Es mayor de e
 Para saber si un numero esta en cierto rango podemos usar el && (AND)
 Si esta entre tanto && tanto
 */
+
+/* let edad = Number(prompt("ingrese su edad"));
+if (edad < 16) {
+    console.log("es menor de edad");
+} 
+else if (edad >= 16 && edad < 18) {
+    console.log("casi deja de ser menor de edad");
+} 
+else {
+    console.log("es mayor de edad");
+}
+ */
+
+/* 
+Solicitar al usuario un numero A (Asumimos que el usuario escribira correctamente un numero)
+Solicitar al usuario un numero B (Asumimos que el usuario escribira correctamente un numero)
+Solicitar al usuario una operacion 
+
+Si la operacion es "+" o "sumar" entonces deberemos por consola mostrar el resultado de la suma de A + B es C
+Si la operacion es '-' o "restar" entonces deberemos por consola mostrar el resultado de la resta de A - B es C 
+Si la operacion no es ninguno de los anteriores deberemos decir por consola "Operacion invalida, vuelva a intentar"
+
+*/
+
+/* let numeroA = Number(prompt('Ingrese un numero'))
+let numeroB = Number(prompt('Ingrese otro numero'))
+let operacion = prompt('Ingrese una operacion')
+if(operacion === '+' || operacion === 'sumar'){
+    console.log(numeroA + numeroB)
+}
+else if(operacion === '-' || operacion === 'restar'){
+    console.log(numeroA - numeroB)
+}
+else{
+    console.log('Operacion invalida, vuelva a intentar')
+}
+ */
