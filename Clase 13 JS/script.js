509c1141f06d47db4e26769d6d28c56b20a75b55
matu.dev <mati.a.gimenez2002@gmail.com>
2026-07-29 09:55:47 -0300

    Clase 13

---
+++ Clase 13 JS/script.js
@@ -0,0 +1,55 @@
+/* 
+CONDICIONES
+Estructuras que van a permitir controlar el flujo de nuestro codigo
+
+IF: el if permite controlar si un bloque de codigo se ejecuta o no bajo una cierta condicion
+ELSE: Va siempre seguido de un IF o ELSE IF y se ejecuta cuando a la condicion de este NO sea verdadera
+ELSE IF: Va siempre seguido de un IF o ELSE IF y te permite agregar una condicion cuando la condicion de este falle
+*/
+
+let password = prompt('ingrese su contraseña')
+
+let contraseniaCorrecta = password === 'pepe_123'
+
+if(contraseniaCorrecta){
+    console.log("bienvenido!")
+}
+else{
+    console.log("Credenciales invalidas")
+}
+
+
+let numero_dia = 2
+
+if(numero_dia === 0){
+    console.log("Hoy es lunes")
+}
+else if(numero_dia === 1){
+    console.log("Hoy es martes")
+}
+else if(numero_dia === 2){
+    console.log("Hoy es miercoles")
+}
+else if(numero_dia === 3){
+    console.log("Hoy es jueves")
+}
+else if(numero_dia === 4){
+    console.log("Hoy es viernes")
+}
+else if(numero_dia === 5){
+    console.log("Hoy es sabado")
+}
+else{
+    console.log("Hoy es domingo")
+}
+
+
+/* 
+Solicitar al usuario su edad (Imaginemos para practicos que el dato siempre es valido (un numero positivo))
+Si la edad del usuario es menor a 16 entonces indicar por consola "Es menor de edad"
+Si la edad del usuario es entre 16 y 18 entonces indicar por consola "Casi deja de ser menor de edad"
+Si la edad del usuario es mayor a 18 entonces indicar por consola "Es mayor de edad"
+
+Para saber si un numero esta en cierto rango podemos usar el && (AND)
+Si esta entre tanto && tanto
+*/
