79b4c5ffd3d0ee29a738a7f1cc3c9b295e1a0f0c
matu.dev <mati.a.gimenez2002@gmail.com>
2026-08-19 09:29:04 -0300

    Clase 19 JS

---
+++ Clase 19 JS/script.js
@@ -0,0 +1,38 @@
+
+let productos = [
+    {
+        id: 1,
+        titulo: 'Silla oficina',
+        precio: 320000,
+        stock: 10
+    },
+    {
+        id: 2,
+        titulo: 'Escritorio madera',
+        precio: 120000,
+        stock: 3
+    },
+    {
+        id: 3,
+        titulo: 'Alfombra roja',
+        precio: 60000,
+        stock: 7
+    }
+]
+/* 
+Dada una lista de productos que actuara como estado 
+    -   Crear una funcion renderProducts que tomara la lista y la mostrara en pantalla
+        Cada producto seguira la sig estructura:
+                <div>
+                    <h2>Titulo</h2>
+                    <div><b>Precio:</b> $precio</div>
+                    <div><b>Stock:</b> $stock</div>
+                    <button>Eliminar</button>
+                    <button>Editar</button>
+                </div>
+        Si no hay productos decir en un <p>Lista de productos vacia</p>.
+        Esta funcion deberia ser invocada una vez asi renderizamos la lista de productos.
+
+    - Crear la funcion setProductos (valor) y al llamarla cambiara el valor del estado y volvera a renderizar la lista de productos
+        Para probar este setter podrian llamar a setProductos([]) y en pantalla deberian ver el parrafo indicando que la lista esta vacia
+*/
