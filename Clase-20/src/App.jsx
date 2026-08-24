02c57026cbc90d8803c4b4d8a6b03e48c7514df5
matu.dev <mati.a.gimenez2002@gmail.com>
2026-08-24 11:54:38 -0300

    Clase 20

--- Clase-20/src/App.jsx
+++ Clase-20/src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import sumar,{ PI, restar } from './math.js'
+
+console.log(sumar(PI, PI))
+
 export default function App() {
   return (
-    <div>Hola, esta es mi primera app en React!!!</div>
+    <div>
+      <h1>Hola mundo!</h1>
+      <a>Click aqui</a>
+      
+    </div>
   )
 }
