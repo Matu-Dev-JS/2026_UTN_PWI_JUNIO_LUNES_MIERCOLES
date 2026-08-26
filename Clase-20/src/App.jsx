fefe171943b39803e39a872d9caaafe7ee0fe47a
matu.dev <mati.a.gimenez2002@gmail.com>
2026-08-26 10:45:26 -0300

    Clase 21

--- Clase-20/src/App.jsx
+++ Clase-20/src/App.jsx
@@ -13,9 +13,22 @@ export default function App() {
         El valor del iva de ${precio} ARS es ${precio * 0.21} ARS
       </div>
       {/* instancia del componente PorductCard, a esto le decimos llamarlo "as a component" */}
-      <ProductCard/> 
-      <ProductCard/> 
-      <ProductCard/> 
+      <ProductCard 
+        title={"Tv Samsung 32\""}
+        price={300000}
+        description={"Tv Samsung 32\" con resolucion Full HD"}
+        id='1'
+      /> 
+      <ProductCard 
+        title={"Tv samsung 42\""}
+        price={400000}
+        description={"Tv Samsung 42\" con resolucion Full HD"}
+      /> 
+      <ProductCard 
+        title={"Tv samsung 52\""} 
+        price={600000}
+        description={"Tv Samsung 52\" con resolucion Full HD"}
+      /> 
     </div>
   )
 }
--- Clase-20/src/Components/ProductCard/ProductCard.jsx
+++ Clase-20/src/Components/ProductCard/ProductCard.jsx
@@ -2,21 +2,38 @@
 Un componente es una funcion que retorna JSX (html)
 Un componente por buenas practicas deben ser nombrados en UpperCamelCase
 Para instanciar un componente usamos la sintaxis "as a component"/"como componente"
+Para poder trasmitir informacion a un componente un padre puede dar informacion al componente mediante las props
+Estas props siempre seran un objeto
 */
-function ProductCard() {
-    const product = {
-        title: '',
-        price: 0,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aut ea repellendus. Cupiditate, dolores pariatur? Excepturi rem maiores explicabo, minus reprehenderit recusandae ratione possimus earum voluptatem nesciunt, nam, aut exercitationem!'
-    }
+
+/* 
+title (obligatoria)
+price (obligatoria)
+description (obligatoria)
+*/
+function ProductCard(propiedades) {
+    console.log("propiedades de product card son:", propiedades)
+
+    
     return (
         <div>
-            <h2>{product.title}</h2>
-            <p>{product.description}</p>
-            <div>Precio: ${product.price} ARS</div>
+            <h2>{propiedades.title}</h2>
+            <p>{propiedades.description}</p>
+            <div>Precio: ${propiedades.price} ARS</div>
             <button>Comprar</button>
             <hr />
         </div>
     )
 }
 export default ProductCard
+
+
+//Declaracion
+//En la declaracion determinamos que valores vamos a recibir (parametros)
+function sumar (a, b){
+    return a + b
+}
+
+//invocacion
+sumar(1, 1)
+sumar(2, 2)
