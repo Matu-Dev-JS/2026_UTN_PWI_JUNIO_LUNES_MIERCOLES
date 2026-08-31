4bceb68b96b29ddf9e182a8f15112e322e485df0
matu.dev <mati.a.gimenez2002@gmail.com>
2026-08-31 11:48:34 -0300

    Clase 22 React Estados

--- Clase-20/src/Components/Counter/Counter.jsx
+++ Clase-20/src/Components/Counter/Counter.jsx
@@ -6,7 +6,11 @@ const Counter = () => {
     const [count, setCount] = useState(0)
     function handleIncrement() {
         //Mi estado contador ahora vale lo que valia antes mas 1
-        setCount(count + 1)
+        setCount(
+            (prevCounter) => {
+                return prevCounter + 1
+            }
+        )
     }
     const handleDecrement = () => { 
         //Mi estado contador ahora vale lo que valia antes menos 1
@@ -14,10 +18,19 @@ const Counter = () => {
     }
 
     console.log('[Counter.jsx] Me renderize')
+
+    /* 
+    Contador parte 2:
+    Si el contador vale 10 poner un mensaje en rojo indicando 'Has alcanzado el limite' y deshabilitar el boton de sumar
+    Si el contador vale 0: 
+        - debe haber un boton de comprar unicamente (este al dar click incrementara el contador)
+        - No estaran el boton de +, - o el contador
+    
+    */
     return (
         <div>
             <h2>Contador: {count}</h2>
-            <button onClick={handleDecrement}>-</button>
+            <button onClick={handleDecrement} disabled={false}>-</button>
             <button onClick={handleIncrement}>+</button>
         </div>
     )
