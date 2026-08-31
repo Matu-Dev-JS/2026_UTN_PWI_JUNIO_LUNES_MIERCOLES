449d5f81e9c2044c5251d7c1ed553c02b1f7813b
matu.dev <mati.a.gimenez2002@gmail.com>
2026-08-31 10:21:53 -0300

    Clase 22 React Estados

--- Clase-20/src/App.jsx
+++ Clase-20/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './global.css'
 import ProductCard from './Components/ProductCard/ProductCard'
 import Message from './Components/Message/Message'
@@ -6,66 +6,49 @@ import Message from './Components/Message/Message'
 
 export default function App() {
 
-  let precio = 40000
+  /* 
+  useState es una funcion de react que te permite declarar estados
+  useState recibe el valor incial de tu estado
+  useState retorna un array de 2 posiciones (tupla)
+    La primera posicion representa el valor del estado en si
+    La segunda posicion del array representa la funcion de setteo (la funcion para actualizar el estado)
+
+  LOS ESTADOS SON INMUTABLES, si queremos modificar/actualizar el valor de un estado debemos hacerlo mediante el setter
+  */
+  /* const modalAbiertoEstado = useState(false)
+  const modalAbierto = modalAbiertoEstado[0]
+  const setModalAbierto = modalAbiertoEstado[1] */
+  const [modalAbierto, setModalAbierto] = useState(false)
+
+
+  function abrirModal (){
+    setModalAbierto(true)
+  }
+
+  console.log('[App.jsx] Me renderize')
   return (
     <div>
-      <div className='product-card'>
-        hola mundo
-      </div>
-      <h1>Hola mundo!</h1>
-      <a>Click aqui</a>
-      <div>
-        El valor del iva de ${precio} ARS es ${precio * 0.21} ARS
-      </div>
-      {/* instancia del componente PorductCard, a esto le decimos llamarlo "as a component" */}
-      <ProductCard
-        title={"Tv Samsung 32\""}
-        price={300000}
-        description={"Tv Samsung 32\" con resolucion Full HD"}
-        isOnlyForMembers={true}
-        id='1'
-      />
-      <ProductCard
-        title={"Tv samsung 42\""}
-        price={40}
-        description={"Tv Samsung 42\" con resolucion Full HD"}
-        isOnlyForMembers={false}
-      />
-      <ProductCard
-        title={"Tv samsung 52\""}
-        price={600000}
-        description={"Tv Samsung 52\" con resolucion Full HD"}
-        isOnlyForMembers={true}
-      />
-      <div className="pantalla-chat">
+      <button onClick={abrirModal}>Abrir modal</button>
+      {
+        modalAbierto
+        &&
+        <div className='modal-container'>
+          <div className='modal'>
+            <h1>Hola soy un modal</h1>
+            <button>Cerrar</button>
+          </div>
+        </div>
+      }
 
-
-        <Message
-          autor={"Yo"}
-          contenido={"Hello, i was wondering when my order will be shipped?"}
-          fecha={"1:30 PM"}
-          estatus_envio={"visto"}
-        />
-        <Message
-          autor={"Maria"}
-          contenido={"Your order has been processed and we are preparing..."}
-          fecha={"1:32 PM"}
-          estatus_envio={"enviado"}
-        />
-        <Message
-          autor={"Yo"}
-          contenido={"Thank you"}
-          fecha={"1:32 PM"}
-          estatus_envio={"visto"}
-        />
-        <Message
-          autor={"Maria"}
-          contenido={"Your order has been shipped and is spected to be..."}
-          fecha={"12:00 PM"}
-          estatus_envio={"enviado"}
-        />
-      </div>
     </div>
   )
 }
 
+
+
+const persona_1 = ['pepe', 'suarez', 50]
+
+const [nombre, apellido, edad] = persona_1
+/* const persona_nombre = persona_1[0]
+const persona_apellido = persona_1[1]
+const pesona_edad = persona_1[2] */
