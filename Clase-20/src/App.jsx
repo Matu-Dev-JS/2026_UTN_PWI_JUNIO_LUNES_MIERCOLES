0de976c3215b5321dbbffcc85ea835141c0567ed
matu.dev <mati.a.gimenez2002@gmail.com>
2026-09-02 11:59:31 -0300

    Clase 23 Consumo de APIs

--- Clase-20/src/App.jsx
+++ Clase-20/src/App.jsx
@@ -1,59 +1,89 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './global.css'
 import ContactsList from './Components/ContactsList/ContactsList'
 
 
 
 export default function App() {
-
-  const mensajes_servidor = [
-    {
-      id: 1,
-      autor: 'Jose',
-      contenido: 'Hola!!',
-      fecha: '26 agosto 2026 11:45'
-    },
-    {
-      id: 2,
-      autor: 'Maria',
-      contenido: 'Todo bien?! 😎',
-      fecha: '26 agosto 2026 11:55'
-    },
-    {
-      id: 3,
-      autor: 'Pedro',
-      contenido: 'Que cuentan?',
-      fecha: '26 agosto 2026 11:56'
-    }
-
-  ]
+  const [response, setResponse] = useState(null)
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
   /* 
-  En react tenemos listas de JSX
-  Las listas de JSX son arrays donde cada elemento tiene un expresion JSX
-  Las listas JSX se renderizan directamente en pantalla de forma ordenada
+  JS maneja la asincronia mediante promesas (un objeto especial)
+  Una promesa tiene 3 estados posibles: 
+    Pendiente - Mientras la operacion asicronica aun no se resolvio
+      Resuelta - Cuando la promesa se resolvio efectivamente 
+      Rechazada - Si la promesa no se pudo resolver (hubo un error)
   */
-  const mensajes_jsx = []
 
-  for (const mensaje of mensajes_servidor) {
-    mensajes_jsx.push(
-      <div key={mensaje.id}>
-        <h2>{mensaje.autor}</h2>
-        <p>{mensaje.contenido}</p>
-        <span>Fecha: {mensaje.fecha}</span>
-        <hr />
-      </div>
+  /* 
+  Una funcion que ordena al navegador hacer una consulta HTTP
+  fetch es una funcion asicronica y me va a devolver el resultado de la consulta
+  */
+  const resultado = fetch(
+    'https://jsonplaceholder.typicode.com/posts',
+    {
+      method: "GET"
+    }
+  )
+
+  //Esto muestra promise pending porque para el momento en el que se resolvio console.log la promesa aun no estaba resuelta
+  //Console.log al ser sincronico NO espera a que la promesa se resuelva
+  console.log('Inmediatamente muestro el resultado', resultado)
+
+
+
+  async function consultarAlServidor() {
+    const respuesta_http = await fetch(
+      'https://jsonplaceholder.typicode.com/posts',
+      {
+        method: "GET"
+      }
+    )
+    console.log("Aguarde a que la tarea se resuelva y muestro el resultado", respuesta_http)
+    const resultado = await respuesta_http.json()
+    console.log(resultado)
+    setIsLoading(false)
+    setResponse(resultado)
+  }
+
+  
+
+  //useEffect te permite manejar cuantas veces se ejecuta una funcion
+  //Para que se ejecute una vez se hace de la sig manera
+  useEffect(
+    () => {
+      consultarAlServidor()
+    },
+    []
+  )
+  console.log(isLoading, response)
+  if(isLoading){
+    return (
+      <div>Cargando...</div>
     )
   }
 
-
-
+  const lista_posteos_jsx = []
+  for(const post of response){
+    lista_posteos_jsx.push(
+      <div key={post.id}>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+        <hr/>
+      </div>
+    )
+  }
   return (
     <div>
-      {mensajes_jsx}
-      <ContactsList/>
+      {lista_posteos_jsx}
+
     </div>
   )
 }
+
+
+
 
 /* 
 Contactos:
@@ -73,3 +103,47 @@ Renderizar la lista de contactos
 No es necesario hacer estilos
 Ejemplo: https://user-images.githubusercontent.com/44744039/224526440-b4eb6a54-4dda-430c-a681-286f5b406fe7.png
 */
+
+
+
+/* 
+  /* const mensajes_servidor = [
+    {
+      id: 1,
+      autor: 'Jose',
+      contenido: 'Hola!!',
+      fecha: '26 agosto 2026 11:45'
+    },
+    {
+      id: 2,
+      autor: 'Maria',
+      contenido: 'Todo bien?! 😎',
+      fecha: '26 agosto 2026 11:55'
+    },
+    {
+      id: 3,
+      autor: 'Pedro',
+      contenido: 'Que cuentan?',
+      fecha: '26 agosto 2026 11:56'
+    }
+
+  ] */
+/*
+En react tenemos listas de JSX
+Las listas de JSX son arrays donde cada elemento tiene un expresion JSX
+Las listas JSX se renderizan directamente en pantalla de forma ordenada
+*/
+/*   const mensajes_jsx = []
+
+  for (const mensaje of mensajes_servidor) {
+    mensajes_jsx.push(
+      <div key={mensaje.id}>
+        <h2>{mensaje.autor}</h2>
+        <p>{mensaje.contenido}</p>
+        <span>Fecha: {mensaje.fecha}</span>
+        <hr />
+      </div>
+    )
+  } */
+
+
