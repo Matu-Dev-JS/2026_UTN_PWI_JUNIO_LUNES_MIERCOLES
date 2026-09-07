7400d341aea3f7203fba7d5c1135c5d70025cd0c
matu.dev <mati.a.gimenez2002@gmail.com>
2026-09-07 11:59:32 -0300

    Clase 24

--- Clase-20/src/Components/LoginForm/LoginForm.jsx
+++ Clase-20/src/Components/LoginForm/LoginForm.jsx
@@ -1,6 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginForm() {
+    
+    const [formState, setFormState] = useState({email: '', password: ''})
+
+    function handleSubmit (evento){
+        evento.preventDefault() 
+        const form = evento.target 
+        const email = form.email_login.value
+        const password = form.password.value
+    }
+
+    function handleChangeInput (evento) {
+        const campo = evento.target //hace referencia al campo que el usuario esta modificando
+        const nombre_campo = campo.name
+        const valor_campo = campo.value
+        setFormState(
+            (prevFormState) => {
+                const cloned_state = {...prevFormState} //spread operator
+                cloned_state[nombre_campo] = valor_campo
+                return cloned_state
+            }
+        )
+    }
+    console.log(formState)
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>Iniciar sesion</h1>
+            <div>
+                <label htmlFor='email'>Email:</label>
+                <br/>
+                <input 
+                    type='email' 
+                    name='email' 
+                    id='email' 
+                    value={formState.email} 
+                    onChange={handleChangeInput}
+                />
+            </div>
+            <div>
+                <label htmlFor='password'>Contraseña:</label>
+                <br/>
+                <input 
+                    type='password' 
+                    name='password' 
+                    id='password' 
+                    value={formState.password}
+                    onChange={handleChangeInput}
+                />
+            </div>
+            <button type='submit'>Iniciar sesion</button>
+        </form>
+    )
+}
+
+
+/* 
+En react existen 2 tipos de formularios
+Los normales:
+    Capturamos su valor al enviar el formulario.
+Controlados:
+    Capturamos su valor al cambiar de valor cualquiera de sus campos, basicamente tenemos un estado que en tiempo real captura cada minimo cambio en el formulario
+*/
+
+/* 
+Ejemplo de formulario NO controlado
+*/
+/* export default function LoginForm() {
     function handleSubmit (evento){
         evento.preventDefault() //Evita que se recarge la pagina
         const form = evento.target //target es una referencia del elemento de donde se desencadeno el evento
@@ -24,13 +90,4 @@ export default function LoginForm() {
             <button type='submit'>Iniciar sesion</button>
         </form>
     )
-}
-
-
-/* 
-En react existen 2 tipos de formularios
-Los normales:
-    Capturamos su valor al enviar el formulario.
-Controlados:
-    Capturamos su valor al cambiar de valor cualquiera de sus campos, basicamente tenemos un estado que en tiempo real captura cada minimo cambio en el formulario
-*/
+} */
