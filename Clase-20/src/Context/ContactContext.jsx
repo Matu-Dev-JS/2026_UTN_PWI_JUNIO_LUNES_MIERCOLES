5e4666b2bb784eb716ec488d580a734698ccd216
matu.dev <mati.a.gimenez2002@gmail.com>
2026-09-16 11:26:03 -0300

    Clase 27

---
+++ Clase-20/src/Context/ContactContext.jsx
@@ -0,0 +1,11 @@
+import contact_list_server from "../data/contact-data-mook";
+
+/* 
+Crear un contexto llamado ContactContext que tenga un estado interno de contacts
+Este contexto debera ser a nivel de rutas
+Las rutas que afectara son:
+    - /
+    - /contact/:contact_id
+
+Pueden probarlo rapidamente haciendo un console.log en los componentes afectados, para revisar si efectivamente llega la informacion.
+*/
